e25b18e61ec99d1318be6c7b190f860036e4cd7a
wario01 <timothywario00@gmail.com>
2025-10-05 18:17:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.2.0",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/wario01/Knightbot-MD/edit/main/settings.js",
 };
 
 module.exports = settings;
